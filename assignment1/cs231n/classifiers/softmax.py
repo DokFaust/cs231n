6e84c6530a6574efc736d0f1320179ae477b71ca
DoktorFaust <dokfaust@autistici.org>
2017-01-17 19:33:08 +0100

    SVM loss with nested loops

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -23,13 +23,60 @@ def softmax_loss_naive(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
+
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+
+  D = W.shape[0]
+  C = W.shape[1]
+  N = X.shape[0]
+
+  for i in xrange(N):
+      #We consider samples column-wise, sample is 1xD
+      sample = X[i,:]
+
+      scores = sample.dot(W)
+      max_score = np.argmax(scores)
+
+      #To avoid numerical instability, now our score values will lay
+      #between [-max_score, 0]
+
+      scores -= max_score
+      exp_sum = np.sum(np.exp(scores))
+
+      #Loss is updated for each sample and normalized
+      #see course notes for the numerical stability of this formula
+
+      loss += -scores[y[i]] + np.log(exp_sum)
+
+      #Here comes the harsh stuff.
+      #We have to update dW for each class
+      #First we consider if we are grading the correct class, in that
+      #case we add minus one, then we add our score value.
+      #The score value is raised to e then normalized over
+      #the sum of exponentials
+
+      #Anyway the result of the expression between parentheses is just
+      # a scalar so, sample has size 1xD and dW[:,j] Dx1
+
+      exp_sum = np.sum(np.exp(scores))
+
+      for j in xrange(C):
+          dW[:,j] += sample * (-1 * (j==y[i]) + np.exp(scores[j])/exp_sum )
+
+  #Loss and dW are normalized over the number of training examples
+
+  loss /= N
+  dW /= N
+
+  #regularization, for loss is l2
+
+  loss += reg*np.sum(W**2) / 2
+  dW += reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -59,4 +106,3 @@ def softmax_loss_vectorized(W, X, y, reg):
   #############################################################################
 
   return loss, dW
-
